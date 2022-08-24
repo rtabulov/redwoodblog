817224c612e063adeb24ad24e0dd0e793ea8f63f
Rassul <rasul.tabulov@gmail.com>
2022-08-24 18:32:11 +0600

    something

---
+++ api/src/graphql/contacts.sdl.ts
@@ -0,0 +1,32 @@
+export const schema = gql`
+  type Contact {
+    id: String!
+    name: String!
+    email: String!
+    message: String!
+    createdAt: DateTime!
+  }
+
+  type Query {
+    contacts: [Contact!]! @requireAuth
+    contact(id: String!): Contact @requireAuth
+  }
+
+  input CreateContactInput {
+    name: String!
+    email: String!
+    message: String!
+  }
+
+  input UpdateContactInput {
+    name: String
+    email: String
+    message: String
+  }
+
+  type Mutation {
+    createContact(input: CreateContactInput!): Contact! @requireAuth
+    # updateContact(id: String!, input: UpdateContactInput!): Contact! @requireAuth
+    # deleteContact(id: String!): Contact! @requireAuth
+  }
+`
--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,14 +1,28 @@
 import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import {
   FieldError,
   Form,
+  FormError,
   Label,
   Submit,
   SubmitHandler,
   TextAreaField,
   TextField,
 } from '@redwoodjs/forms'
+import {
+  CreateContactMutation,
+  CreateContactMutationVariables,
+} from 'types/graphql'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 interface FormValues {
   name: string
@@ -17,15 +31,29 @@ interface FormValues {
 }
 
 const ContactPage = () => {
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data)
+  const [create, { loading, error }] = useMutation<
+    CreateContactMutation,
+    CreateContactMutationVariables
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your submission!')
+    },
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    const res = await create({ variables: { input: data } })
+    console.log({ res })
   }
 
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
+      <Toaster />
+
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }} error={error}>
+        <FormError error={error} wrapperClassName="form-error" />
+
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -41,13 +69,7 @@ const ContactPage = () => {
         </Label>
         <TextField
           name="email"
-          validation={{
-            required: true,
-            pattern: {
-              value: /^[^@]+@[^.]+\..+$/,
-              message: 'Please enter a valid email address',
-            },
-          }}
+          validation={{ required: true }}
           errorClassName="error"
         />
         <FieldError name="email" className="error" />
@@ -62,7 +84,7 @@ const ContactPage = () => {
         />
         <FieldError name="message" className="error" />
 
-        <Submit>Send</Submit>
+        <Submit disabled={loading}>Send</Submit>
       </Form>
     </>
   )
